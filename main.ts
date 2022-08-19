7719d77a4d6dc42d4130cec4c96287a179a55545
THATRavenclaw <111553515+THATRavenclaw@users.noreply.github.com>
2022-08-19 12:00:35 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        led.plot(2, 2)
+    } else {
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        led.unplot(2, 2)
+        basic.clearScreen()
+    }
+    if (pins.digitalReadPin(DigitalPin.P2) == 1) {
+        led.plot(4, 4)
+    } else {
+        led.unplot(4, 4)
+    }
+})
